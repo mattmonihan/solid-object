52df119b8945f300327ca0bb1a4ce58ee0e73247
Matt Monihan <mmonihan@gmail.com>
2015-03-02 02:39:11 -0500

    Added a tab so you can grab the markup.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require formbuilder/vendor/js/vendor
 //= require formbuilder/dist/formbuilder
 //= require_tree .
