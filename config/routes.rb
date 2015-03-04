7dc513dc99015756342d22c4bbe48775c9ed6fc2
Matt Monihan <mmonihan@gmail.com>
2015-03-03 22:21:32 -0500

    Updated root route to go to model_definition#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'model_definition#index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  
+
   get 'home/index'
 
   resources :model_definition
